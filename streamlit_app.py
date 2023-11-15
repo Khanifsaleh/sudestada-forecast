a3bb299f7eed390dd4a658f04fb9891d78124a88
Khanifsaleh <khanif.saleh@gmail.com>
2023-11-15 08:04:16 +0700

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import pandas as pd
 
-st.write('Hello world!')
+st.write('Sudestada Forecast')
+
+@st.cache_data
+def load_data():
+    df = pd.read_excel('data_for_presentation.xlsx')
+    return df
+df = load_data()
+st.dataframe(df)
